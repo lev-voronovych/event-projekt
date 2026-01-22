f78a7b55eb781ab13158a955e822ad4b94957846
lev-voronovych <vlev0760@gmail.com>
2026-01-22 16:38:41 +0200

    function get img

--- src/index.js
+++ src/index.js
@@ -1,31 +1,47 @@
 key ='JHFGnXEDch2MFGcn1xSI30ZApMODKhwX'
-baseURL = 'https://app.ticketmaster.com/discovery/v2/events.json?size=21&apikey='
-
+baseURL = 'https://app.ticketmaster.com/discovery/v2/events.json'
+pageLimit = 20 ;
 const eventListContainer = document.querySelector('.event-list');
 
 async function getEvents() {
     try {
-        let response = await fetch(baseURL + key);
+        let response = await fetch(`${baseURL}?apikey=${key}&classificationName=music&page=0&size=${pageLimit}&source=universe`);
 
           if (!response.ok) {
             throw new Error('HTTP error: ' + response.status);
         }
         
       let data = await response.json();
-      console.log(data)
+      console.log(data._embedded.events)
       return data;
     } catch (error){
         console.error(error)
     }
 }
 
+// '4_3';
+// '3_2';
+
+function getEventImg(event) {
+  const img =
+    event.images.find(img => img.ratio === '4_3') ||
+    event.images.find(img => img.ratio === '3_2') ||
+    event.images[0]
+
+  return img.url;
+}
+
+
+
 function renderEvents(events) {
   const markup = events.map((event) => {
+    const imgUrl = getEventImg(event);
+     
     return `<li class="event-item">
-    
+    <img alt='' src="${imgUrl}"></img>
     <p class="event-name">${event.name}</p>
-<p class="event-time">${event.dates.start.localDate}</p>    
-<p class="event-location">${event._embedded.venues[0].name}  </p>    
+    <p class="event-time">${event.dates.start.localDate}</p>    
+    <p class="event-location">${event._embedded.venues[0].name}  </p>    
     </li>`;
   }).join("")
 
